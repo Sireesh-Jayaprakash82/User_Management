0c43be3aa994300a1f3e8217ada62c48c3aa6283
Sireesh Jayaprakash <sireesh.jayaprakash@happiestminds.com>
2023-03-16 12:28:30 +0530

    Added functionalities to user and role models

--- user_management/app/controllers/users_controller.rb
+++ user_management/app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: %i[edit]
+
+  def edit
+    if @user.update(user_params)
+      render json: @user, status:200
+    else
+      render json: {error: "Not updated"}
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:full_name, :email, :username, :password)
+  end
+
+  def find_user
+    @user = User.find_by(id: params[:id])
+  end
+
 end
