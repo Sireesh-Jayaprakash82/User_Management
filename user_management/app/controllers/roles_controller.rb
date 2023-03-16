efc711fa726e78a0ad8e2c19fa772c8f0e4ddfe1
Sireesh Jayaprakash <sireesh.jayaprakash@happiestminds.com>
2023-03-16 16:20:48 +0530

    JWT and Serialization

--- user_management/app/controllers/roles_controller.rb
+++ user_management/app/controllers/roles_controller.rb
@@ -1,27 +1,51 @@
 class RolesController < ApplicationController
   before_action :find_role, only: %i[show]
+  before_action :authorize_request
   
   def index
     @roles = Role.all
     if @roles
-      render json: @roles, status:200
+      render json: {data: @roles}, status:200
     else
       render json: {error: "No roles"}
     end
   end
 
-  def show
-    if @role
-      render json: @role, status:200
+  def create
+    @role=Role.new(params.require(:role).permit(:role_name))
+    if @role.save
+        render json: {data: @role}
     else
-      render json: {error: "Role does not exist"}
+        render json: {status: "deleting FAILED"}
+    end
+  end
+
+  def show
+    @user = @role.users
+    if @user
+      render json: {data: @user}, status:200
+    else
+      render json: {error: "No users assigned to this role"}
+    end
+  end
+
+  def destroy
+    @role=Role.find(params[:id])
+    if @role.destroy
+        render json: {status: "deleted successfully"}
+    else
+        render json: {status: "deleting FAILED"}
     end
   end
 
   private
 
   def find_role
-    @role = Role.find_by(id: params[:id])
+    @role = Role.find(params[:id])
+  end
+
+  def role_params
+    params.require(:role).permit(:role_name)
   end
 
 end
--- user_management/app/controllers/users_controller.rb
+++ user_management/app/controllers/users_controller.rb
@@ -1,11 +1,43 @@
 class UsersController < ApplicationController
   before_action :find_user, only: %i[edit]
+  before_action :authorize_request, except: %i[create]
+
+  def index 
+    @users = User.all
+    render json: {data: @users}, status:200
+  end
+
+  def create
+    @role=Role.find_by(role_name: role_params) 
+    @user = User.new(user_params)
+    @user.roles << @role
+    if @user.save
+      render json: {data: [ @user, @role ]}
+    end
+  end
+
 
   def edit
     if @user.update(user_params)
-      render json: @user, status:200
+      render json: {data: @user}, status:200
     else
       render json: {error: "Not updated"}
+    end
+  end
+
+
+  def show
+    @user = User.find(params[:id])
+    @roleofuser = @user.roles
+    render json: {data: @user ,roleassigned: @roleofuser}
+  end
+
+  def destroy
+    @User=User.find(params[:id])
+    if @User.destroy
+        render json: {status: "User deleted successfully"}
+    else
+        render json: {status: "deleting FAILED"}
     end
   end
 
@@ -15,8 +47,12 @@ class UsersController < ApplicationController
     params.require(:user).permit(:full_name, :email, :username, :password)
   end
 
+   def role_params 
+    params.require(:user).require(:role)
+  end
+
   def find_user
-    @user = User.find_by(id: params[:id])
+    @user = User.find(params[:id])
   end
 
 end
