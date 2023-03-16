a117cc4b32189ebf89255f368a0db74dc4ce233a
resham661 <reshamgg12@gmail.com>
2023-03-16 12:13:42 +0530

    adding index in user

--- user_management/app/controllers/users_controller.rb
+++ user_management/app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    # @roles = Role.new
+    @users = @role.users.all
+    render json: @users
+  end
+
+  def update
+
+    
+  end
 end
